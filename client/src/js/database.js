0099ef5e4a5538469902c3566a235149021a7788
Rockojoe2 <lee.d.joey@gmail.com>
2023-08-28 21:19:21 -0500

    FIXED ERROR IDK HOW

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,40 +14,32 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  try {
-    console.log('Post to the database');
+  
+    console.log('Put to the database');
 
-    const jateDb = await openDB('jate', 1);
+    const contactDb = await openDB('jate', 1);
 
-    const tx = jateDb.transaction('jate', 'readwrite');
+    const tx = contactDb.transaction('jate', 'readwrite');
 
     const store = tx.objectStore('jate');
 
-    const request = store.add({ content: content });
+    // const request = store.add({ content: content });
+
+    const request = store.put({value: content});
 
     const result = await request;
 
     console.log('Data saved to the database', result);
-
-  }
-  catch
-  {
-    console.error('putDb not implemented');
-  }
-
-
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
 
-  try
-  {
     console.log("GET from the database");
 
-    const jateDb = await openDB("jate", 1);
+    const contactDb = await openDB("jate", 1);
 
-    const tx = jateDb.transaction("jate", "readonly");
+    const tx = contactDb.transaction("jate", "readonly");
 
     const store = tx.objectStore("jate");
 
@@ -55,13 +47,7 @@ export const getDb = async () =>{
 
     const result = await request;
     console.log('result.value', result);
-    return result;
-
-  }
-  catch
-  {
-    console.error('getDb not implemented');
-  }
+    return result?.value;
 
 };
 
